1196a244d840530eb52d235841a93fa68d7f27e3
ClintWestwoodPL <39919370+ClintWestwoodPL@users.noreply.github.com>
2019-01-14 00:06:48 +0100

    Add files via upload

---
+++ RokPrzestepny.java
@@ -0,0 +1,28 @@
+package _zadania;
+
+import java.util.Scanner;
+
+public class RokPrzestepny {
+
+    public static void main(String[] args) {
+
+        int rok;
+
+        System.out.println("Sprawdźmy czy rok jest przestępny\n");
+        System.out.println("Podaj rok: ");
+
+        Scanner rokPrzestepny = new Scanner(System.in);
+        rok = rokPrzestepny.nextInt();
+
+        if(rok%4==0 && rok%100!=0 || rok%400==0){
+            System.out.println("Rok jest przestępny!");
+        }
+        else{
+            System.out.println("Rok nie jest przestepny!");
+        }
+
+    }
+
+
+    }
+
