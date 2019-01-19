dad1e6ee1bdca8306737b4e5a2ce31b3722e9bb0
Tomasz Kucybała <tkucybala@me.com>
2019-01-19 13:04:49 +0100

    Patryk

---
+++ src/_8_funkcje/FunkcjeGeometryczne.java
@@ -0,0 +1,12 @@
+package _8_funkcje;
+
+public class FunkcjeGeometryczne {
+
+    public static double poleKwadratu(double a) {
+        return a*a;
+    }
+
+    public static double poleKoła (double r) {
+        return Math.PI * r * r;
+    }
+}
---
+++ src/_8_funkcje/ProgramBezFunkcji.java
@@ -0,0 +1,34 @@
+package _8_funkcje;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ProgramBezFunkcji {
+    public static void main(String[] args) {
+        Scanner wejscie = new Scanner(System.in);
+        wejscie.useLocale(Locale.US);
+
+        System.out.println("Podaj liczbę: ");
+        double x = wejscie.nextDouble();
+
+        double poleKwadratu = x * x;
+        System.out.println("Pole kwadratu: " + poleKwadratu);
+
+        double poleKola = Math.PI * x *x ;
+        System.out.println("Pole koła: " + poleKola);
+
+        System.out.println("A teraz wylosuję liczbę");
+
+        double losowa = Math.random() * 100;
+
+        System.out.println(losowa);
+
+        double poleKwadratu2 = losowa * losowa;
+        System.out.println("Pole kwadratu: " + poleKwadratu);
+
+        double poleKola2 = Math.PI * losowa * losowa ;
+        System.out.println("Pole koła losowa: " + poleKola);
+
+    }
+
+}
